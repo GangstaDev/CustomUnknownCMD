51a6a83914ba4939907fa061cad9a137bef05cc5
GangstaMods <brendan.gb@hotmail.com>
2025-09-16 21:40:08 -0400

    Initial commit

---
+++ src/main/java/org/gangsta/customUnknownCMD/CustomUnknownCMD.java
@@ -0,0 +1,56 @@
+package org.gangsta.customUnknownCMD;
+
+import java.io.File;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.help.HelpTopic;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.gangsta.customUnknownCMD.util.ColorUtil;
+import org.gangsta.customUnknownCMD.commands.UnknownCMDCommand;
+
+public class CustomUnknownCMD extends JavaPlugin implements Listener {
+    FileConfiguration config;
+    File cfile;
+    private String Message;
+
+    public void onEnable() {
+        this.config = getConfig();
+        this.config.options().copyDefaults(true);
+
+        if (!this.config.contains("message")) {
+            this.config.set("message", "&cUnknown Command!");
+        }
+
+        saveConfig();
+        this.cfile = new File(getDataFolder(), "config.yml");
+
+        loadConfigValues();
+
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+
+        UnknownCMDCommand commandExecutor = new UnknownCMDCommand(this);
+        getCommand("unknowncmd").setExecutor(commandExecutor);
+    }
+
+    public void loadConfigValues() {
+        this.Message = ColorUtil.colorize(this.config.getString("message", "&cUnknown Command!"));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        Player p = event.getPlayer();
+        if (!event.isCancelled()) {
+            String command = event.getMessage().split(" ")[0];
+            HelpTopic htopic = Bukkit.getServer().getHelpMap().getHelpTopic(command);
+            if (htopic == null) {
+                p.sendMessage(this.Message);
+                event.setCancelled(true);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/gangsta/customUnknownCMD/commands/UnknownCMDCommand.java
@@ -0,0 +1,129 @@
+package org.gangsta.customUnknownCMD.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.gangsta.customUnknownCMD.CustomUnknownCMD;
+import org.gangsta.customUnknownCMD.util.ColorUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UnknownCMDCommand implements CommandExecutor, TabCompleter {
+
+    private final CustomUnknownCMD plugin;
+
+    public UnknownCMDCommand(CustomUnknownCMD plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("customunkncmd.admin")) {
+            sender.sendMessage(ColorUtil.colorize("&cYou don't have permission to use this command!"));
+            return true;
+        }
+
+        if (args.length == 0) {
+            sendHelp(sender);
+            return true;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "reload":
+                handleReload(sender);
+                break;
+
+            case "set":
+                handleSet(sender, args);
+                break;
+
+            default:
+                sendHelp(sender);
+                break;
+        }
+
+        return true;
+    }
+
+    private void handleReload(CommandSender sender) {
+        plugin.reloadConfig();
+        plugin.loadConfigValues();
+        sender.sendMessage(ColorUtil.colorize("&aConfiguration reloaded successfully!"));
+    }
+
+    private void handleSet(CommandSender sender, String[] args) {
+        if (args.length < 2) {
+            sender.sendMessage(ColorUtil.colorize("&cUsage: /unknowncmd set <message>"));
+            return;
+        }
+
+        // Join all arguments after "set" to form the message
+        StringBuilder messageBuilder = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            messageBuilder.append(args[i]);
+            if (i < args.length - 1) {
+                messageBuilder.append(" ");
+            }
+        }
+
+        String newMessage = messageBuilder.toString();
+
+        // Update config
+        plugin.getConfig().set("message", newMessage);
+        plugin.saveConfig();
+        plugin.loadConfigValues();
+
+        sender.sendMessage(ColorUtil.colorize("&aUnknown command message set to: " + ColorUtil.colorize(newMessage)));
+    }
+
+    private void sendHelp(CommandSender sender) {
+        sender.sendMessage(ColorUtil.colorize("&6=== &eCustomUnknownCMD Help &6==="));
+        sender.sendMessage(ColorUtil.colorize("&e/unknowncmd reload &7- Reload the configuration"));
+        sender.sendMessage(ColorUtil.colorize("&e/unknowncmd set <message> &7- Set the unknown command message"));
+        sender.sendMessage(ColorUtil.colorize("&7Example: /unknowncmd set &cUnknown Command!"));
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!sender.hasPermission("customunknowncmd.admin")) {
+            return new ArrayList<>();
+        }
+
+        if (args.length == 1) {
+            List<String> completions = new ArrayList<>();
+            String input = args[0].toLowerCase();
+
+            for (String subcommand : Arrays.asList("reload", "set")) {
+                if (subcommand.startsWith(input)) {
+                    completions.add(subcommand);
+                }
+            }
+
+            return completions;
+        }
+
+        if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
+            List<String> examples = Arrays.asList(
+                    "&cUnknown command!",
+                    "&#FFD700&lUnknown command!",
+                    "&l&gradient:#FFD700:#FFA500:#FF8C00:#FF6347&Unknown command!"
+            );
+
+            List<String> completions = new ArrayList<>();
+            String input = args[1].toLowerCase();
+
+            for (String example : examples) {
+                if (example.toLowerCase().startsWith(input)) {
+                    completions.add(example);
+                }
+            }
+
+            return completions;
+        }
+
+        return new ArrayList<>();
+    }
+}
